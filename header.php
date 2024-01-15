62f0e99b42b1ab1323d862a2cacc1a529b9996f5
root <sebastien.journiac@le-campus-numerique.fr>
2024-01-15 14:54:50 +0100

    On crée le header.php

---
+++ header.php
@@ -0,0 +1,6 @@
+ <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>CV de Sébastien Journiac</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
