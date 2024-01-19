c247fb11ceae2ae502df67dae170ec90c891160a
Sébastien Journiac <sebastien.journiac@le-campus-numerique.fr>
2024-01-19 14:16:31 +0100

    On factorise l'index

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 $_GET["page"] = filter_input(INPUT_GET,"page", FILTER_SANITIZE_URL);
+$Hobby=filter_input(INPUT_GET,'Hobby', FILTER_SANITIZE_URL);
+$CV=filter_input(INPUT_GET,'CV', FILTER_SANITIZE_URL);
+$contact=filter_input(INPUT_GET,'contact', FILTER_SANITIZE_URL);
 if ($_GET["page"] == "CV") {
     include 'CV.php';
 } else if ($_GET["page"] == "Hobby") {
@@ -9,4 +12,8 @@ if ($_GET["page"] == "CV") {
 } else if (!$_GET["page"]) {
     echo "erreur 404, Fais un effort !";
 }
-
+$routes=array(
+    'Hobby.php'=> $Hobby,
+    'contact.php'=> $contact,
+    'CV.php'=>$CV
+);
