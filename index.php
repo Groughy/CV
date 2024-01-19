7234770456850778667d6a80b4c2017bb9ba9371
Sébastien Journiac <sebastien.journiac@le-campus-numerique.fr>
2024-01-19 14:55:15 +0100

    On factorise l'index

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
 <?php
-$_GET["page"] = filter_input(INPUT_GET,"page", FILTER_SANITIZE_URL);
+$page = filter_input(INPUT_GET,"page", FILTER_SANITIZE_URL);
 $Hobby=filter_input(INPUT_GET,'Hobby', FILTER_SANITIZE_URL);
 $CV=filter_input(INPUT_GET,'CV', FILTER_SANITIZE_URL);
 $contact=filter_input(INPUT_GET,'contact', FILTER_SANITIZE_URL);
-if ($_GET["page"] == "CV") {
-    include 'CV.php';
-} else if ($_GET["page"] == "Hobby") {
-    include 'Hobby.php';
-} else if ($_GET["page"] == "contact") {
-    include 'contact.php';
-} else if (!$_GET["page"]) {
-    echo "erreur 404, Fais un effort !";
-}
+
 $routes=array(
-    'Hobby.php'=> $Hobby,
-    'contact.php'=> $contact,
-    'CV.php'=>$CV
-);
+    'Hobby.php'=> "Hobby",
+    'contact.php'=> "contact",
+    'CV.php'=> "CV");
+foreach ($routes as $key => $value){
+if ($page == $value){
+    include "./".$key;
+}else{
+    echo "404 PAS TROUVE, VA AILLEURS !";
+}
+};
